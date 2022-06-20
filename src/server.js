8dfcbf507f7b1602e621ec6f4120c4fbb64404f7
Nayan P Ingale <97461960+NayanIngale1@users.noreply.github.com>
2022-06-20 19:27:40 +0530

    Assingment done

--- src/server.js
+++ src/server.js
@@ -1,8 +1,59 @@
 // install and import express
-const express = () => {};
+const path = require('path')
+const userJson = require(path.join(__dirname,'/assets/user.json'))
+const express = require('express');
 let app = express();
 
 // Code here
+
+app.get("/",(req,res)=>{
+    try {
+       
+        res.status(200).sendFile(path.join(__dirname,'/assets/users.html'))
+    } catch (error) {
+        console.log('error:', error)
+        res.send(500).send(error.message);
+        
+    }
+})
+app.get("/users",(req,res)=>{
+    try {
+        res.status(200).sendFile(path.join(__dirname,'/assets/user.json'))
+    } catch (error) {
+        console.log('error:', error)
+        res.send(500).send(error.message);
+        
+    }
+})
+
+app.get("/users/:id", (req, res) => {
+    try {
+     
+    const user = userJson.filter((el)=>{if(el.id==req.params.id){return el}});
+        if (user.length>0) {
+            res.status(200).send(user[0]);
+        } else {
+            res.status(404).send("No user Found");
+        }
+  } catch (error) {
+    console.log("error:", error);
+    res.send(500).send(error.message);
+  }
+});
+app.post("/users",(req,res)=>{
+    try {
+        const user = userJson.create(req.body)
+        res.status(200).send(user)
+    } catch (error) {
+        console.log('error:', error)
+        res.send(500).send(error.message);
+        
+    }
+})
+
+app.listen(8000,async()=>{
+    await console.log("listening on port 8000");
+})
 
 // Note: Do not remove this export statement
 module.exports = app;
